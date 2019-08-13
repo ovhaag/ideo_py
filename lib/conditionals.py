f5ab8e72bf3571967edb9971d0512a4ce97fb4ae
Oliver Haag <oliver@haagnetz.de>
2019-08-13 14:45:06 +0200

    expanded "if"

--- lib/conditionals.py
+++ lib/conditionals.py
@@ -6,19 +6,40 @@ condtionals: if statement
 @author: ovinci
 """
 
-def chained_comparison(small, medium, large):
-    # harmful
-    # if x <= y and y <= z:
-    #     return True 
+def is_seq(small, medium, large):
+    """chained_comparison""" 
     #
-    # ideomatic
+    # harmful: explicit chain with "and"
+    # ideomatic:  chained_comparison (direct, without and)
     if small <= medium <= large:
         return True 
     
+def note_for(flag):
+    """ternary_operator_replacement"""
+    #
+    # Python has no ternary operator (e.g. “x ? True : False”), so
+    # ideomatic: use if, else
+    note = 'flag was true' if flag else 'flag was false'
+    return note
+
+def word_in_text(word, text):
+    """use in"""
+    #
+    # harmful: compound if
+    # ideomatic: use "in"
+    words = text.split(' ')
+    return word in words
+
 def main():
-    c = chained_comparison(1, 12, 123)
-    print(c)
-    
-    
+    s, m, l = 1, 22, 333
+    up = is_seq(s, m, l)
+    note = note_for(up)
+    found = word_in_text("true", note)
+    if found:
+        print("happy")
+    else: 
+        print("sad")
+        
 if __name__ == '__main__':
     main()
+    
